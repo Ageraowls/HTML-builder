373dab51c16b47177b86f11b3fdf20ee39b7a464
Andrew Shumeiko <shumeiko.drew@gmail.com>
2022-05-23 20:17:03 +0500

    feat: finish fourth task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,17 @@
+const fs = require('fs/promises');
+const path = require('path');
+const { stdout } = process;
+const dirPath = path.join(__dirname, 'files');
+const dirPathCopy = path.join(__dirname, 'files-copy');
 
+const copyDir = async () => {
+  await fs.rm(dirPathCopy, { recursive: true, force: true });
+  await fs.mkdir(dirPathCopy);
+  const files = await fs.readdir(dirPath, { withFileTypes: true });
+  files.forEach((file) => {
+    fs.copyFile(path.join(dirPath, file.name), path.join(dirPathCopy, file.name));
+  });
+  stdout.write('\nsuccessful copy folder\n');
+};
+
+copyDir();
